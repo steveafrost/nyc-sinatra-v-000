d54d548bf2e8b3beca839c226781f1866710894f
steveafrost <ffrostt@gmail.com>
2016-07-25 22:14:10 -0400

    finished figures controller - passing tests

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,56 @@
+class FiguresController < ApplicationController
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+
+    if params[:title][:name]
+      @figure.titles << Title.create(params[:title])
+    end
+
+    if params[:landmark][:name]
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    @figure.save
+
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  post '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+
+    if params[:title][:name]
+      @figure.titles << Title.create(params[:title])
+    end
+
+    if params[:landmark][:name]
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,8 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :"/landmarks/index"
+  end
+
 end

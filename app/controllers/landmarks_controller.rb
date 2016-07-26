501a049e2f59a9c92cf9e6a4e7c609c41577eba4
steveafrost <ffrostt@gmail.com>
2016-07-25 23:18:09 -0400

    finished landmarks controller - passing tests

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -5,4 +5,30 @@ class LandmarksController < ApplicationController
     erb :"/landmarks/index"
   end
 
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks' do
+    Landmark.create(params[:landmark])
+    redirect to '/landmarks'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params[:landmark])
+
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
 end
